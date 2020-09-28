519e7129bf40536c10d275b049cbc6d9778cf2a7
Carl Summers <31711543+CSumm@users.noreply.github.com>
2020-09-28 10:26:43 -0400

    Add all files from project

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+       let sectionLoader = document.getElementById('section-loader');
+    
+    setTimeout(() => {
+        sectionLoader.style.display = 'none';
+    }, 3000);
+}
